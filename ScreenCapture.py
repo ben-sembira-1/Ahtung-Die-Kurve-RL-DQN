ba07f31b83e5e63a0ed3b53bcc1fecd5dce430ca
ben-sembira <62756833+ben-sembira@users.noreply.github.com>
2020-03-31 23:04:38 +0300

    many bad attempts for getting auhtung code from internet

--- ScreenCapture.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path='C:/bin/chromedriver.exe')
-driver.get('http://forum.curvefever.com/play.html')
-driver.save_screenshot("screenshot.png")
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 """
 imports:
 """
-import numpy as np
-import gym
+# import numpy as np
+# import gym
+#
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation, Flatten
+# from keras.optimizers import Adam
+#
+# from rl.agents.dqn import DQNAgent
+# from rl.policy import EpsGreedyQPolicy
+# from rl.memory import SequentialMemory
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-from keras.optimizers import Adam
+from selenium import webdriver
+from constants import *
 
-from rl.agents.dqn import DQNAgent
-from rl.policy import EpsGreedyQPolicy
-from rl.memory import SequentialMemory
+from selenium_helper import ScreenCapture
+from selenium.webdriver.chrome.options import Options
 
 ENVIRONMENT_NAME = 'Achtung-v1.0'
+
+s = ScreenCapture()
+options = Options()
+options.add_experimental_option("prefs",CHROME_PREFERENCES)
+driver = webdriver.Chrome(executable_path='C:/bin/chromedriver.exe', options=options)
+s.set_flash(driver)
