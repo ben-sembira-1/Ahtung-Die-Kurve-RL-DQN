dbfe0a523dcf704a2d89002519d60fe25a94d213
ben-sembira <62756833+ben-sembira@users.noreply.github.com>
2020-04-06 13:42:25 +0300

    123

--- main.py
+++ main.py
@@ -38,54 +38,54 @@ dqn_agent.fit(env, nb_steps=5000, visualize=False, verbose=2)
 #dqn_agent.test(env, EPISODES, visualize=True)
 
 # Iterate the game
-for e in range(EPISODES):
-
-    # reset state in the beginning of each game
-    state = env.reset()
-    state = np.reshape(state, [1, 4])
-    ################################################################
-    # REMEMBER TO CHANGE THE RESHAPE WHEN ENV.RESET IS DEFINED
-    ################################################################
-
-    # time_t represents each frame of the game
-    # Our goal is to keep the pole upright as long as possible until score of 500
-    # the more time_t the more score
-    for time_t in range(NUMBER_OF_TURNS_TO_LIVE):
-
-        # turn this on if you want to render
-        # env.render()
-
-        # Decide action
-        action = agent.act(state)
-
-        # Advance the game to the next frame based on the action.
-        # Reward is 1 for every frame the pole survived
-        next_state, reward, done, _ = env.step(action)
-        next_state = np.reshape(next_state, [1, 4])
-        ################################################################
-        # REMEMBER TO CHANGE THE RESHAPE WHEN ENV.RESET IS DEFINED
-        ################################################################
-
-        # memorize the previous state, action, reward, and done
-        agent.memorize(state, action, reward, next_state, done)
-        #IF ABOVE DOESNT WORK CONSIDER REPLACE MEMORIZE WITH REMEMBER
-
-        # make next_state the new current state for the next frame.
-        agent.replay()
-        agent.target_train()
-
-        state = next_state
-
-        # done becomes True when the game ends
-        # ex) The agent drops the pole
-        # if game_over:
-        #     # print the score and break out of the loop
-        #     print("EPISODESS: {}/{}, score: {}"
-        #           .format(time, EPISODESs, time_t))
-        #
-        #     break
-
-    # train the agent with the experience of the EPISODES
+# for e in range(EPISODES):
+#
+#     # reset state in the beginning of each game
+#     state = env.reset()
+#     state = np.reshape(state, [1, 4])
+#     ################################################################
+#     # REMEMBER TO CHANGE THE RESHAPE WHEN ENV.RESET IS DEFINED
+#     ################################################################
+#
+#     # time_t represents each frame of the game
+#     # Our goal is to keep the pole upright as long as possible until score of 500
+#     # the more time_t the more score
+#     for time_t in range(NUMBER_OF_TURNS_TO_LIVE):
+#
+#         # turn this on if you want to render
+#         # env.render()
+#
+#         # Decide action
+#         action = dqn_agent.act(state)
+#
+#         # Advance the game to the next frame based on the action.
+#         # Reward is 1 for every frame the pole survived
+#         next_state, reward, done, _ = env.step(action)
+#         next_state = np.reshape(next_state, [1, 4])
+#         ################################################################
+#         # REMEMBER TO CHANGE THE RESHAPE WHEN ENV.RESET IS DEFINED
+#         ################################################################
+#
+#         # memorize the previous state, action, reward, and done
+#         dqn_agent.memorize(state, action, reward, next_state, done)
+#         #IF ABOVE DOESNT WORK CONSIDER REPLACE MEMORIZE WITH REMEMBER
+#
+#         # make next_state the new current state for the next frame.
+#         dqn_agent.replay()
+#         dqn_agent.target_train()
+#
+#         state = next_state
+#
+#         # done becomes True when the game ends
+#         # ex) The agent drops the pole
+#         # if game_over:
+#         #     # print the score and break out of the loop
+#         #     print("EPISODESS: {}/{}, score: {}"
+#         #           .format(time, EPISODESs, time_t))
+#         #
+#         #     break
+#
+#     # train the agent with the experience of the EPISODES
 
 
 def get_action(self, states):
